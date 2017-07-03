3faeb8992ea7b586ff4506ca082900e2a6b988f1
yash <yashthakar1997@gmail.com>
2017-07-03 23:06:33 +0530

    changed index ui
    designed front end using css

--- index.php
+++ index.php
@@ -1,5 +1,53 @@
-<?php
+<html>
+<head>
+    <title>e maintenence</title>
+    <link rel="stylesheet" type="text/css" href="css/global.css" />
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,devanagari" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width,initial-scale: 1.0, user-scalabe=0">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/general.js"></script>
+</head>
+<body>
 
-echo "<h1>hello world";
+    <div id="header">
+        <div class="logo"><a href="#"> e <span>Maintanence</span></a></div>
+    </div>    
 
-?>
+    <div><a class="mobile" href="#">Menu</a></div>
+
+    <div id="container">
+        <div class="sidebar">
+        <ul id="nav">
+            <li><a class="selected" href="#">HOME</a></li>
+            <li><a href="#">search</a></li>
+            <li><a href="#">princial</a></li>
+            <li><a href="#">teacher</a></li>
+        </ul>    
+        </div>
+        <div class="content">
+            <h1>sdfs</h1>
+            <p> here you can do things</p>
+            <div id="box">
+                <div class="box-top"> NEWS </div>
+                <div class="box-panel">
+                    Thos os some text lalala
+                </div>
+            </div>
+            <div id="box">
+                <div class="box-top"> NEWS </div>
+                <div class="box-panel">
+                    Thos os some text lalala
+                </div>
+            </div>
+            <div id="box">
+                <div class="box-top"> NEWS </div>
+                <div class="box-panel">
+                    Thos os some text lalala asdna ajs ja dsj asd jas dj asdj asjd jas djas dja sdj asjd jas dj djas djsa djas 
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
