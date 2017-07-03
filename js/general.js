4034e7047977ecd7b80ca18bcef1be4dea54de18
yash <yashthakar1997@gmail.com>
2017-07-03 23:25:55 +0530

    made mistake

--- js/general.js
+++ js/general.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
-	$("a.mobile").click(function(){
+	$("a.mobile").on('click touchstart', (function(){
 		$(".sidebar").slideToggle('fast');
-	
 	});
-
+	
 	window.onresize = function(event) {
 		if($(window).width() > 320) {
 			$(".sidebar").show();
 		}
 	};
-	$("a.mobile").on('click touchstart', (function(){
-		$(".sidebar").slideToggle('fast');
-	});
+
 });
