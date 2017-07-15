aa3c3d3e483233b282ee167d8c2fc3c253494416
yash <yashthakar1997@gmail.com>
2017-07-15 15:55:42 +0530

    included headerfile of pgadmin

--- loginphp.php
+++ loginphp.php
@@ -1,4 +1,11 @@
 <?php
+<?php
+   $host        = "host = ec2-79-125-125-97.eu-west-1.compute.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d7vpp251p4l9tg";
+   $credentials = "user = baialgsjdhvmxy password=57161bba2de05428b22372402d30d2e1f002c39a358e2b2b89cb8b1678342150";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
 
 
 echo "string";
