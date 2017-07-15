28abc231e4c62a364b013743bad6f5152216ae71
yash <yashthakar1997@gmail.com>
2017-07-15 12:38:52 +0530

    added login and signup page
    and trying to connect to pg database

---
+++ login.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+    <title>e maintenence</title>
+    <link rel="stylesheet" type="text/css" href="css/global.css" />
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,devanagari" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width,initial-scale: 1.0, user-scalabe=0">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/general.js"></script>
+    <style type="text/css">
+        
+input[type=text] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: none;
+    background-color: #34495e;
+    color: white;
+}
+button[type=submit] {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
+    width: 40%;
+}
+
+
+    </style>
+</head>
+<body>
+<!-- https://thenewboston.com/videos.php?cat=92&video=19876 -->
+
+    <div id="header">
+        <div class="logo"><a href="#"> e <span>Maintanence</span></a></div>
+    </div>    
+
+    <div><a class="mobile" href="#">Menu</a></div>
+
+    <div id="container">
+        <div class="sidebar">
+        <ul id="nav">
+            <li><a class="selected" href="#">HOME</a></li>
+            <li><a href="#">search</a></li>
+            <li><a href="#">princial</a></li>
+            <li><a href="#">teacher</a></li>
+        </ul>    
+        </div>
+        <div class="content">
+            <h1><span class="slideInRight">Login Form </span></h1>
+            <div id="box">
+                <div class="box-top">Enter Details</div>
+                <div class="box-panel">
+                <div class="input-field">
+                <form action="" method="POST">
+                    
+                     <label for="uname">User Name</label>
+                     <input type="text" id="uname" name="uname" value="" placeholder="enter your name here">
+                     <label for="pass">PassWord</label>
+                     <input type="text" id="pass" name="pass" value="" placeholder="enter your password here">
+                     <br /><br />
+                     <button type="submit"> login </button>
+
+                </form>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ sign.php
@@ -0,0 +1,34 @@
+<?php
+   $host        = "host = ec2-79-125-125-97.eu-west-1.compute.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d7vpp251p4l9tg";
+   $credentials = "user = baialgsjdhvmxy password=57161bba2de05428b22372402d30d2e1f002c39a358e2b2b89cb8b1678342150";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+ $sql =<<<EOF
+      CREATE TABLE user
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      PASSWORD       TEXT     NOT NULL,
+      ROLE        CHAR(50),
+      ACTIVE         INT);
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret) {
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
+?>
+
+
+?>
