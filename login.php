b7a088a9b74518b4ae6403e315bb1ebd6cc4db26
yash <yashthakar1997@gmail.com>
2017-07-15 14:43:36 +0530

    signup

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ button[type=submit] {
                      <input type="text" id="pass" name="pass" value="" placeholder="enter your password here">
                      <br /><br />
                      <button type="submit"> login </button>
-                     <button onclick="window.location.href='http://etask.herokuapp.com/signup.php'">sign up</button>
+                     <button type="submit" onclick="window.location.href='http://etask.herokuapp.com/signup.php'">sign up</button>
 
                 </form>
                 </div>
