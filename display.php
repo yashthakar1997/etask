7c49a70526d378ad0d2be33cde1e8b32213b9f28
yash <yashthakar1997@gmail.com>
2017-07-15 13:40:54 +0530

    drop db

--- display.php
+++ display.php
@@ -5,7 +5,7 @@
    $credentials = "user = baialgsjdhvmxy password=57161bba2de05428b22372402d30d2e1f002c39a358e2b2b89cb8b1678342150";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
-
+/*
  $sql =<<<EOF
       SELECT * from USERS;
 
@@ -21,10 +21,10 @@ EOF;
       echo "NAME = ". $row[1] ."\n";
       echo "PASSWORD = ". $row[2] ."\n\n";
    }
-
-/*   
+*/
+   
 $sql =<<<EOF
-      DROP TABLE USERS;
+      DROP DATABASE d7vpp251p4l9tg;
 EOF;
 
    echo "Operation done successfully\n";
