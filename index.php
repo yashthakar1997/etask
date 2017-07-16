073d569b6a5904e5115d4eba7d6d3afeda1d6ad3
yash <yashthakar1997@users.noreply.github.com>
2017-07-16 19:13:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-   ob_start();
-   session_start();
-   if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == 1) {
-        //session is set
-        header('Location: index.php');
-    } else if(!isset($_SESSION['logged_in']) || (isset($_SESION['logged_in']) && $_SESSION['logged_in'] == 0)){
-        //session is not set
-        header('Location: login.php');
-    }
-?>
-
 <html>
 <head>
     <title>e maintenence</title>
